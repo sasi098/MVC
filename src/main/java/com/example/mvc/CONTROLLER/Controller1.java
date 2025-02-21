0cd9107701cfec5807dd4c3ac6fcf3137e1cfa85
Sasankredd <126907638+Sasankredd@users.noreply.github.com>
2025-02-21 09:40:29 +0530

    FORM

--- src/main/java/com/example/mvc/CONTROLLER/Controller1.java
+++ src/main/java/com/example/mvc/CONTROLLER/Controller1.java
@@ -3,6 +3,9 @@ package com.example.mvc.CONTROLLER;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import java.util.*;
 
 @Controller
@@ -12,5 +15,16 @@ public class Controller1 {
     public String date(Model model){
         model.addAttribute("date1",new java.util.Date());
         return "datehtml";
+    }
+
+    @GetMapping("/getform")
+    public String getform(){
+        return "get-form";
+    }
+
+    @PostMapping("/process-from")
+    public String process(@RequestParam("student") String name,Model model){
+        model.addAttribute("name",name.toUpperCase());
+        return "processform";
     }
 }
